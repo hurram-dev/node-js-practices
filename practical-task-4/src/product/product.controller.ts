16963815e87739433a643dfeff2c3295fd1b15fd
Khurrambek Sadirdinov <khurrambek_sadirdinov@epam.com>
2026-03-01 14:16:10 +0500

    complete api

--- practical-task-4/src/product/product.controller.ts
+++ practical-task-4/src/product/product.controller.ts
@@ -1,5 +1,7 @@
+import { update } from './product.repository';
 import type { Request, Response } from 'express';
-import { getAllProducts } from './product.service';
+import { createProduct, deleteProduct, getAllProducts, updateProduct } from './product.service';
+import { ProductEntity } from './product.model';
 
 export const getAllProductsHandler = async (req: Request, res: Response) => {
     const products = await getAllProducts();
@@ -7,3 +9,24 @@ export const getAllProductsHandler = async (req: Request, res: Response) => {
     res.status(200).json({ data: products})
 }
 
+export const createProductHandler = async (req: Request<any, any, ProductEntity, any>, res: Response) => {
+    const product = await createProduct(req.body);
+
+    res.status(201).json({ data: product})
+}
+
+export const updateProductHandler = async (req: Request<{ id: string }, any, ProductEntity, any>, res: Response) => {
+    const { id } = req.params;
+
+    const updatedProduct = await updateProduct(id, req.body);
+
+    res.status(200).json({ data: updatedProduct})
+}
+
+export const deleteProductHandler = async (req: Request<{ id: string }, any, any, any>, res: Response) => {
+    const { id } = req.params;
+
+    await deleteProduct(id);
+
+    res.status(204).send();
+}
--- practical-task-4/src/product/product.router.ts
+++ practical-task-4/src/product/product.router.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import { createProductHandler, deleteProductHandler, getAllProductsHandler, updateProductHandler } from './product.controller';
+import { validate } from '../middleware/validate.middleware';
+import { productSchema } from './product.model';
+
 
 const router = express.Router();
 
-import { getAllProductsHandler } from './product.controller';
-
 router.get('/', getAllProductsHandler);
+
+router.post('/', validate(productSchema), createProductHandler);
+
+router.patch('/:id', validate(productSchema), updateProductHandler);
+
+router.delete('/:id', deleteProductHandler);
 
 export default router;
